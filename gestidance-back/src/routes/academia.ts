960275605dc255e7bc6d55c72f68abf046141116
AlexGonz27 <alexagustingonzalez@gmail.com>
2025-02-24 21:49:44 -0400

    corrección academia

--- gestidance-back/src/routes/academia.ts
+++ gestidance-back/src/routes/academia.ts
@@ -7,27 +7,33 @@ const router = express.Router(); // Inicialización del router
 const academia = express.Router();
 
 academia.post('/agregar', async (req: Request, res: Response) => {
-    const { nombre_academia,
-            fecha,
-            nombre_director,
-            telefono_director }: 
-        {   nombre_academia: string;
-            fecha: string;
-            nombre_director: string; 
-            telefono_director: string } = req.body;
+    const { academyName,
+            registrationDate,
+            directorName,
+            directorPhone }: 
+        {   academyName: string;
+            registrationDate: string;
+            directorName: string; 
+            directorPhone: string } = req.body;
+
+    try {
+        console.log([academyName]);
+        // Realizamos la consulta para encontrar al usuario en la base de datos
+        const result = await pool.query('INSERT INTO academias(nombre, fechainsc, nombdirect, tlfdirect) VALUES ($1, $2, $3, $4)', [academyName,registrationDate,directorName,directorPhone]);
+
+        res.status(200).json({ message: "Academia ingresada!"});
+    } catch (err) {
+        console.error("Error en la consulta a la base de datos: ", err);
+        res.status(500).json({ error: 'Error al procesar la solicitud' });
+    }
+});
+
+academia.get('/obtener', async (req: Request, res: Response) => {
 
     try {
         // Realizamos la consulta para encontrar al usuario en la base de datos
-        const result = await pool.query('INSERT INTO public.academias(nombre, fechainsc, nombdirect, tlfdirect) VALUES ($1, $2, $3, $4)', [nombre_academia,fecha,nombre_director,telefono_director]);
-
-        // Verificamos si encontramos un usuario con el correo proporcionado
-        if (result.rows.length > 0) {
-            const user = result.rows[0]; // El primer usuario encontrado
-            res.status(200).json({ message: "Academia ingresada!"});
-        } else {
-            // Si no se encuentra el usuario en la base de datos, respondemos con un error
-            res.status(401).json({ message: "Credenciales inválidas (usuario no encontrado)" });
-        }
+        const result = await pool.query('SELECT * FROM academias');
+        res.status(200).json(result.rows);
     } catch (err) {
         console.error("Error en la consulta a la base de datos: ", err);
         res.status(500).json({ error: 'Error al procesar la solicitud' });
